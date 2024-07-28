45c9eb6f7b9853e842d44f1f43c0ad63aa06c120
redowl <esentoki3@gmail.com>
2024-07-28 15:30:35 -0600

    rollback to s5 version

--- src/managers/HistoryManager.java
+++ src/managers/HistoryManager.java
@@ -2,11 +2,15 @@ package managers;
 
 import tasks.Task;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+
 
 public interface HistoryManager {
+
     void add(Task task);
 
-    LinkedList<Task> getHistory();
+    ArrayList<Task> getHistory();
+
+    void remove(Task task);
 
 }
