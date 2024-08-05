e84bd2d5a97dd1f45f8a77c3d9b024e8e0956e06
redowl <esentoki3@gmail.com>
2024-08-05 05:38:33 -0600

    s7 final

--- src/exeptions/ManagerSaveException.java
+++ src/exeptions/ManagerSaveException.java
@@ -2,6 +2,7 @@ package exeptions;
 
 public class ManagerSaveException extends RuntimeException {
     private static final String message = "Произошла ошибка во время работы с файлом";
+
     public ManagerSaveException(Exception e) {
         super(message, e);
     }
